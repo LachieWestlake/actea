756929d025daac160dfd1cbe574b0961872de444
DeeplyDiligent <deep.bhattacharyya@monash.edu>
2019-12-22 17:46:35 +1100

    created new function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,37 @@
+import { Request, Response } from "express";
+
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 
-exports.storeUserData = functions.auth.user().onCreate((user:any) => {
-    console.log('adding user...');
-    console.log(user.toJSON());
-    const name = (user.displayName || user.user_name) || null
-    db.collection("users").doc(user.email).set({
-        uid: user.uid,
-        email: user.email,
-        displayName: name,
-        photoURL: user.photoURL,
-        phoneNumber: user.phoneNumber,
-        createdAt: new Date(user.metadata.creationTime)
+exports.storeUserData = functions.auth.user().onCreate((user: any) => {
+  console.log("adding user...");
+  console.log(user.toJSON());
+  const name = user.displayName || user.user_name || null;
+  db.collection("users")
+    .doc(user.email)
+    .set({
+      uid: user.uid,
+      email: user.email,
+      displayName: name,
+      photoURL: user.photoURL,
+      phoneNumber: user.phoneNumber,
+      createdAt: new Date(user.metadata.creationTime)
     });
-    return true;
+  return true;
 });
+
+exports.landingPageForm = functions.https.onRequest(
+  async (req: Request, response: Response) => {
+    const formData: string = req.query.formData;
+    await db
+        .collection("landingPageForm")
+        .add(JSON.parse(formData));
+    response.set("Access-Control-Allow-Origin", "*");
+    response.status(200).send("Data Added!");
+  }
+);
